8ba4147cf9e0bbe4128cfc500e88eb31932ef982
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-17 17:28:53 +0300

    Add Strings class for using localized strings in the application

---
+++ Movimenta/Utilites/Localization/Strings.swift
@@ -0,0 +1,13 @@
+//
+//  Strings.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/17/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class Strings {
+  
+}
