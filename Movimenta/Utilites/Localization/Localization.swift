3351a00f1eacf7b43ccdc1cd27f10cb815f1f9ca
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-17 17:28:07 +0300

    Add method to be used for localizing strings in the app

--- Movimenta/Utilites/Localization/Localization.swift
+++ Movimenta/Utilites/Localization/Localization.swift
@@ -9,6 +9,15 @@
 import Foundation
 
 final class Localization {
+  class func localize(key: String, defaultValue: String = "", comment: String = "", formatVariables: CVarArg...) -> String {
+    if formatVariables.count > 0 {
+      let localized: String = NSLocalizedString(key, value: defaultValue, comment: comment)
+      return withVaList(formatVariables, {
+        return NSString(format: localized, locale: Locale.application , arguments: $0) as String
+      })
+    }
+    return NSLocalizedString(key, value: defaultValue, comment: comment)
+  }
 }
 
 //MARK: - Application Locale
