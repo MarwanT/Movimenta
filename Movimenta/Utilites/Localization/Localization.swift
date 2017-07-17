dc4212ec1bb9a5c4791ab3358ed540077b3da865
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-17 17:26:33 +0300

    Add Localization class

---
+++ Movimenta/Utilites/Localization/Localization.swift
@@ -0,0 +1,12 @@
+//
+//  Localization.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/17/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class Localization {
+}
