933998ed4a0d1e759a207fee5be2d914b08e905b
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-18 11:28:11 +0300

    Access storyboards view controllers through an enum configuration

---
+++ Movimenta/Storyboard/Storyboard.swift
@@ -0,0 +1,21 @@
+//
+//  Storyboard.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/17/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+enum Storyboard: String {
+  case Root
+  
+  public func instantiate<VC: UIViewController>(_ viewController: VC.Type,
+                          inBundle bundle: Bundle? = nil) -> VC {
+    guard let vc = UIStoryboard(name: self.rawValue, bundle: bundle).instantiateViewController(withIdentifier: VC.storyboardIdentifier) as? VC else {
+      fatalError("Couldn't instantiate \(VC.storyboardIdentifier) from \(self.rawValue)")
+    }
+    return vc
+  }
+}
