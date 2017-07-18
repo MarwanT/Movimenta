26f18249e0cc959276b56d2d1c10003c83595334
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-18 18:21:39 +0300

    Declare font book cases

---
+++ Movimenta/Themes/FontBook.swift
@@ -0,0 +1,29 @@
+//
+//  FontBook.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/18/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+enum FontBook: String {
+  case title1
+  case title2
+  case title3
+  case title4
+  case title5
+  case body1
+  case header1
+  case listTitle
+  case listBody
+  case action1
+  case action2
+  case label1
+  case label2
+  case label3
+  case filter
+  case menu1
+  case menu2
+}
