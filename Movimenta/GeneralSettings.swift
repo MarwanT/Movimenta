21848a4b82be915e3f7cb1c17abcdb88e548aee1
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-19 12:40:45 +0300

    Declare general settings class

---
+++ Movimenta/GeneralSettings.swift
@@ -0,0 +1,16 @@
+//
+//  GeneralSettings.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/19/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+public class GeneralSettings {
+  public static let sharedInstance: GeneralSettings = GeneralSettings()
+  private init() {
+  
+  }
+}
