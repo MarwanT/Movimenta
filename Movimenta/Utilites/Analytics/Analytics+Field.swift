913bba19323b07aaeb6f5d335e18e4d70ab1ce99
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-19 13:31:05 +0300

    Declare Analytics Fields

---
+++ Movimenta/Utilites/Analytics/Analytics+Field.swift
@@ -0,0 +1,15 @@
+//
+//  Analytics+Field.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/19/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+extension Analytics {
+  enum Field: Int {
+    case ApplicationVersion
+  }
+}
