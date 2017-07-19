016e2bf70d3a77b782db0ef7027e751b8b862783
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-19 16:32:05 +0300

    Configure Movimenta objective-c bridging header

---
+++ Movimenta/SupportingFiles/Movimenta-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Movimenta-Bridging-Header.h
+//  Movimenta
+//
+//  Created by Marwan  on 7/19/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+#ifndef Movimenta_Bridging_Header_h
+#define Movimenta_Bridging_Header_h
+
+
+#endif /* Movimenta_Bridging_Header_h */
