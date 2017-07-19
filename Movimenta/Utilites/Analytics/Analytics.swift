a4f0a1b2c10d562956718d73690424827a6c8efb
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-19 11:45:39 +0300

    Declare Analytics class

---
+++ Movimenta/Utilites/Analytics/Analytics.swift
@@ -0,0 +1,13 @@
+//
+//  Analytics.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/19/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+internal final class Analytics {
+  
+}
