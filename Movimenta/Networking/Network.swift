3b29bdc638e67c842f0c7a68523682fe160e0c08
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-21 16:55:44 +0300

    Declare api provider structure

--- Movimenta/Networking/Network.swift
+++ Movimenta/Networking/Network.swift
@@ -37,3 +37,25 @@ public enum APIError: Swift.Error {
 }
 
 public typealias APICompletion = (_ data: Data?,_ statusCode: Int?,_ response: URLResponse?,_ error: APIError?) -> ()
+
+
+/**
+ An endpoint is a semi-internal data structure that Moya uses to reason about the network request that will ultimately be made. An endpoint stores the following data:
+ * The url.
+ * The HTTP method (GET, POST, etc).
+ * The request parameters.
+ * The parameter encoding (URL, JSON, custom, etc).
+ * The HTTP request header fields.
+ * The sample response (for unit testing).
+ 
+ 
+ *Providers map Targets to Endpoints, then map Endpoints to actual network requests.*
+ 
+ **There are two ways that you interact with Endpoints:**
+ * When creating a provider, you may specify a mapping from **Target** to **Endpoint**.
+ * When creating a provider, you may specify a mapping from **Endpoint** to **URLRequest**.
+ */
+
+
+public struct APIProvider {
+}
