d4bd4f6f997f428d45d91d1404c249d8ed82daad
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-21 16:49:22 +0300

    Declare Movimenta API request enum

---
+++ Movimenta/Networking/MovimentaAPI.swift
@@ -0,0 +1,14 @@
+//
+//  MovimentaAPI.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/20/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+public enum MovimentaAPI {
+  case events
+}
