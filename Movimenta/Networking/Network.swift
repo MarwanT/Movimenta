c79cd4b5835a79d0e367b5c258ba76ade7805f7a
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-21 16:52:00 +0300

    Declare 'APIError' type for handling api related errors

--- Movimenta/Networking/Network.swift
+++ Movimenta/Networking/Network.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import Moya
+
+public enum APIError: Swift.Error {
+  case imageMapping(MoyaError)
+  case jsonMapping(MoyaError)
+  case stringMapping(MoyaError)
+  case statusCode(MoyaError)
+  case underlying(MoyaError)
+  case requestMapping(MoyaError)
+  case unidentified
+  
+  init(moyaError: MoyaError) {
+    switch moyaError {
+    case .imageMapping:
+      self = .imageMapping(moyaError)
+    case .jsonMapping:
+      self = .jsonMapping(moyaError)
+    case .requestMapping:
+      self = .requestMapping(moyaError)
+    case .statusCode:
+      self = .statusCode(moyaError)
+    case .stringMapping:
+      self = .stringMapping(moyaError)
+    case .underlying:
+      self = .underlying(moyaError)
+    }
+  }
+}
