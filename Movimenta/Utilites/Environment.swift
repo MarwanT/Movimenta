89ac56f49a7dbddf06cfca4ec867c1e422c169b3
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-21 16:43:32 +0300

    Set the api base URL in the app environment

--- Movimenta/Utilites/Environment.swift
+++ Movimenta/Utilites/Environment.swift
@@ -19,10 +19,12 @@ struct Environment {
   let type: EnvironmentType
   let googleAnalyticsIdentifier: String
   let facebook: (id: String, displayName: String)
+  let apiBaseURL: URL
   
   private init() {
     type = EnvironmentType(rawValue: AppKeys.shared.environmentString) ?? .staging
     googleAnalyticsIdentifier = AppKeys.shared.googleAnalyticsIdentifier
     facebook = (AppKeys.shared.facebookAppId, AppKeys.shared.facebookAppDisplayName)
+    apiBaseURL = URL(string: AppKeys.shared.apiBaseURL)!
   }
 }
