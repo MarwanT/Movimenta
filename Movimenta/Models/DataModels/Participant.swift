e5007dfdfca248d57544a12c07ffb73bd952a526
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-24 18:16:51 +0300

    Define the 'participant' model properties

--- Movimenta/Models/DataModels/Participant.swift
+++ Movimenta/Models/DataModels/Participant.swift
@@ -9,6 +9,28 @@
 import Foundation
 
 struct Participant: ModelCommonProperties {
+  var id: String?
+  var type: ParticipantType
+  var link: URL?
+  var content: String?
+  var title: String?
+  var excerpt: String?
+  var name: String?
+  var firstName: String?
+  var lastName: String?
+  var profession: String?
+  var image: URL?
+  var website: URL?
+  var phone: String?
+  var email: String?
+  var facebook: URL?
+  var twitter: URL?
+  var linkedin: URL?
+  var googleplus: URL?
+  var youtube: URL?
+  var tumblr: URL?
+  var vimeo: URL?
+  var instagram: URL?
 }
 
 extension Participant {
