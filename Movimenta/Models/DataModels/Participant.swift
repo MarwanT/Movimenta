3213f19323b2e50b5ec4bcf7c9731ba6d51c1ee7
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-24 18:13:38 +0300

    Declare 'Participant' model

---
+++ Movimenta/Models/DataModels/Participant.swift
@@ -0,0 +1,12 @@
+//
+//  Participant.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/23/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+struct Participant: ModelCommonProperties {
+}
