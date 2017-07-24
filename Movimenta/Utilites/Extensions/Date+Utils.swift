773934371891fa388f8dd12ea7a84a40f4c1c38f
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-24 18:53:25 +0300

    Add method to get a Date object out a String

---
+++ Movimenta/Utilites/Extensions/Date+Utils.swift
@@ -0,0 +1,29 @@
+//
+//  Date+Utils.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/24/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+  static func formatter(_ format: String = "yyyy-MM-dd HH:mm:ss") -> DateFormatter {
+    let formatter = DateFormatter()
+    formatter.dateFormat = format
+    formatter.locale = Locale.application
+    
+    return formatter
+  }
+  
+  static func from(_ string: String, formatter: DateFormatter? = nil) -> Date? {
+    let dateFormatter = formatter != nil ? formatter! : Date.formatter()
+    
+    guard let date = dateFormatter.date(from: string) else {
+      return nil
+    }
+    return date
+  }
+  
+}
