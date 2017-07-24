3fa8d2be4b9b60e80db0eeeb6ebea4d3ac0e8793
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-24 18:37:49 +0300

    Define Model Common Properties protocol

---
+++ Movimenta/Models/DataModels/ModelCommonProperties.swift
@@ -0,0 +1,17 @@
+//
+//  ModelCommonProperties.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/21/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+protocol ModelCommonProperties {
+  var id: String? { get }
+  var link: URL? { get }
+  var title: String? { get }
+  var content: String? { get }
+  var excerpt: String? { get }
+}
