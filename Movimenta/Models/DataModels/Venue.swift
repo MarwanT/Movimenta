ae2324ad87d479e4aa5908116007de594da9372f
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-24 18:20:09 +0300

    Define 'Venu' model properties

--- Movimenta/Models/DataModels/Venue.swift
+++ Movimenta/Models/DataModels/Venue.swift
@@ -6,7 +6,27 @@
 //  Copyright © 2017 Keeward. All rights reserved.
 //
 
+import CoreLocation
 import Foundation
 
-struct Venue {
+struct Venue: ModelCommonProperties {
+  var id: String?
+  var link: URL?
+  var content: String?
+  var title: String?
+  var excerpt: String?
+  var name: String?
+  var address: String?
+  var city: String?
+  var state: String?
+  var country: String?
+  var website: URL?
+  var phone: String?
+  var mapLink: URL?
+  var email: String?
+  var zipcode: String?
+  var image: URL?
+  var mapAddress: String?
+  var coordinates: CLLocationCoordinate2D?
+}
 }
