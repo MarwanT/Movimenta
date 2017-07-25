1783d2e9d31f892c1db03e2bf140b91a353b89db
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-25 13:48:31 +0300

    Declare the data manager singleton

---
+++ Movimenta/Models/DataModels/Tools/DataManager.swift
@@ -0,0 +1,15 @@
+//
+//  DataManager.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/21/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct DataManager {
+  static let shared = DataManager()
+  private init() {}
+}
