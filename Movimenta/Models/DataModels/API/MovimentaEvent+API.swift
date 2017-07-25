ff2a6d76e378c451197918f499829f720236ec39
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-25 13:02:41 +0300

    Add struct for holding movimenta event api calls

---
+++ Movimenta/Models/DataModels/API/MovimentaEvent+API.swift
@@ -0,0 +1,12 @@
+//
+//  MovimentaEvent+API.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/25/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+struct MovimentaEventAPI {
+}
