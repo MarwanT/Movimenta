f4a74fd303cfecdd1c7e82d37720d964d980f8ec
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-25 17:13:38 +0300

    Declare the Persistence layer singleton

---
+++ Movimenta/Models/DataModels/Tools/Persistence.swift
@@ -0,0 +1,15 @@
+//
+//  Persistence.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/25/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+struct Persistence {
+  static let shared = Persistence()
+  private init() {
+  }
+}
