3aa5df97e6cea4b4f0ae0afdc69f6d76d4d3a4fb
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-07-27 13:23:27 +0300

    Add the app theme protocol

---
+++ Movimenta/Themes/Theme.swift
@@ -0,0 +1,13 @@
+//
+//  Theme.swift
+//  Movimenta
+//
+//  Created by Marwan  on 7/27/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+protocol Theme {
+  func initialize()
+}
