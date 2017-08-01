67b0085353f08a312268e2387e8cecc60b102064
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-01 12:29:01 +0300

    Add the tab bar view controller as the application root vc

---
+++ Movimenta/Controllers/ViewControllers/RootViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RootViewController.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/1/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITabBarController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
+//MARK: Instance
+extension RootViewController {
+  static func instance() -> RootViewController {
+    return Storyboard.Root.instantiate(RootViewController.self)
+  }
+}
