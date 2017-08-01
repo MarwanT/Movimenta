5975b91219c17a1020d99bf18486c08a7e32adb5
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-01 17:04:53 +0300

    Add a UIImage extension method for generating a UIImage out of a color

---
+++ Movimenta/Utilites/Extensions/Image+Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Image+Utils.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/1/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+public extension UIImage {
+  public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+    let rect = CGRect(origin: .zero, size: size)
+    UIGraphicsBeginImageContextWithOptions(rect.size, false, 1.0)
+    color.setFill()
+    UIRectFill(rect)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    guard let cgImage = image?.cgImage else { return nil }
+    self.init(cgImage: cgImage)
+  }
+}
