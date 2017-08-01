a4be9822bf6602e6fc3b4818a67a526ab21ed35c
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-01 17:06:33 +0300

    Add method to root vc for initializing tab bar view controllers

--- Movimenta/Controllers/ViewControllers/RootViewController.swift
+++ Movimenta/Controllers/ViewControllers/RootViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class RootViewController: UITabBarController {
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    initializeTabBarViewControllers()
+  }
+  
+  func initializeTabBarViewControllers() {
   }
 }
 
