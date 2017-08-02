dd23f19f59761a14cfd32da518c70a27cc2cf0b6
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-02 13:02:52 +0300

    Add the events map view model

---
+++ Movimenta/Models/ViewModels/EventsMapViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EventsMapViewModel.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/2/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class EventsMapViewModel {
+  
+}
