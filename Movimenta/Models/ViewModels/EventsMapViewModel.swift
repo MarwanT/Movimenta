63924869a5b03299d99b32010184935d94cb3ae1
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-03 15:00:20 +0300

    Track selected event in events map view model

--- Movimenta/Models/ViewModels/EventsMapViewModel.swift
+++ Movimenta/Models/ViewModels/EventsMapViewModel.swift
@@ -13,6 +13,23 @@ typealias MapEvent = (event: Event, marker: GMSMarker)
 
 final class EventsMapViewModel {
   var mapEvents: [MapEvent] = []
+  var selectedMapEvent: MapEvent? = nil {
+    didSet {
+      /**
+       Updating the marker 'tracksViewChanges' property is required
+       To reflect the image change upon changing value of the isHighlighted flag.
+       */
+      
+      oldValue?.marker.tracksViewChanges = true
+      selectedMapEvent?.marker.tracksViewChanges = true
+      
+      oldValue?.marker.iconImageView?.isHighlighted = false
+      selectedMapEvent?.marker.iconImageView?.isHighlighted = true
+      
+      oldValue?.marker.tracksViewChanges = false
+      selectedMapEvent?.marker.tracksViewChanges = false
+    }
+  }
   
   func loadEvents() {
     mapEvents = DataManager.shared.events.flatMap { (event) -> MapEvent? in
@@ -23,5 +40,23 @@ final class EventsMapViewModel {
       return (event, marker)
     }
     //TODO: Filter events
+  }
+  
+  /** 
+   Returns boolean value indicating wether the tapped marker is
+   being selected or unselected
+   */
+  func updateMapEventSelection(for marker: GMSMarker) -> Bool {
+    guard let mapEvent = mapEvents.filter({ $0.marker === marker }).first else {
+      return false
+    }
+    
+    if mapEvent.event == selectedMapEvent?.event { // Unselect
+      selectedMapEvent = nil
+    } else { // Select
+      selectedMapEvent = mapEvent
+    }
+
+    return selectedMapEvent != nil
   }
 }
