65fe565f751cbb15475d5150f5bb20246df69b1d
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-03 14:55:45 +0300

    Add method for loafing events in 'events map' view model

--- Movimenta/Models/ViewModels/EventsMapViewModel.swift
+++ Movimenta/Models/ViewModels/EventsMapViewModel.swift
@@ -12,5 +12,16 @@ import GoogleMaps
 typealias MapEvent = (event: Event, marker: GMSMarker)
 
 final class EventsMapViewModel {
+  var mapEvents: [MapEvent] = []
   
+  func loadEvents() {
+    mapEvents = DataManager.shared.events.flatMap { (event) -> MapEvent? in
+      guard let position = event.coordinates else {
+        return nil
+      }
+      let marker = GMSMarker.movimentaMarker(position: position)
+      return (event, marker)
+    }
+    //TODO: Filter events
+  }
 }
