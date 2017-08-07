31b2b20fe178acef0dc6850b3dee24c3a83d7cf9
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-07 13:35:23 +0300

    Declare 'Event Details Peek View'  component

---
+++ Movimenta/Components/EventDetailsPeekView/EventDetailsPeekView.swift
@@ -0,0 +1,12 @@
+//
+//  EventDetailsPeekView.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/7/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailsPeekView: UIView {
+}
