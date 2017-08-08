dbdd347d15726048480f0504f536eb51a014712e
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-08 13:54:59 +0300

    Declare event details vc

---
+++ Movimenta/Controllers/ViewControllers/EventDetailsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EventDetailsViewController.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/8/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailsViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
