836b3fba83deedb739c30353655be5ead03b98a7
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-09 16:06:17 +0300

    Add outlets for subviews

--- Movimenta/Controllers/ViewControllers/EventDetailsViewController.swift
+++ Movimenta/Controllers/ViewControllers/EventDetailsViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class EventDetailsViewController: UIViewController {
+  @IBOutlet weak var detailsStackView: UIStackView!
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var categoriesLabel: UILabel!
+  @IBOutlet weak var participantsLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
+  
   var viewModel = EventDetailsViewModel()
   
   static func instance() -> EventDetailsViewController {
