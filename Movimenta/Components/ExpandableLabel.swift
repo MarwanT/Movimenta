297318b5d6843f4a89215907e78d8351b1c59d31
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-10 20:56:04 +0300

    Define the expandable label class subclass of TTTAttributedLabel

---
+++ Movimenta/Components/ExpandableLabel.swift
@@ -0,0 +1,13 @@
+//
+//  ExpandableLabel.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/10/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+import TTTAttributedLabel
+
+class ExpandableLabel: TTTAttributedLabel {
+}
