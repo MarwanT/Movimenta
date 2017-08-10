763529903d4df2f8e4feea687bc6e2430d10ba11
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-10 21:04:20 +0300

    Add "View More" at the end when text is truncated

--- Movimenta/Components/ExpandableLabel.swift
+++ Movimenta/Components/ExpandableLabel.swift
@@ -13,6 +13,35 @@ class ExpandableLabel: TTTAttributedLabel {
   override func awakeFromNib() {
     super.awakeFromNib()
     delegate = self
+  }
+  
+  override var attributedText: NSAttributedString! {
+    willSet {
+      setTruncationToken()
+    }
+  }
+}
+
+//MARK: Helpers
+extension ExpandableLabel {
+  fileprivate func setTruncationToken() {
+    let viewMoreAttributedString = NSMutableAttributedString()
+    let threeDots = NSAttributedString(string: "...", attributes:
+      [
+        NSFontAttributeName : font,
+        NSForegroundColorAttributeName : textColor
+      ]
+    )
+    let viewMore = NSAttributedString(string: Strings.view_more(), attributes:
+      [
+        NSForegroundColorAttributeName : ThemeManager.shared.current.color2,
+        NSFontAttributeName : ThemeManager.shared.current.font12
+      ]
+    )
+    viewMoreAttributedString.append(threeDots)
+    viewMoreAttributedString.append(viewMore)
+    attributedTruncationToken = viewMoreAttributedString
+  }
 }
 
 //MARK: TTTAttributedLabelDelegate
