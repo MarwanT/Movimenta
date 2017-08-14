b1e3a48d82ac8b1c8276cf4b649aa885cd237856
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-14 17:50:08 +0300

    Link subviews with outlets

--- Movimenta/Components/EventDetailsHeaderView/EventDetailsHeaderView.swift
+++ Movimenta/Components/EventDetailsHeaderView/EventDetailsHeaderView.swift
@@ -9,4 +9,11 @@
 import UIKit
 
 class EventDetailsHeaderView: UIView {
+  @IBOutlet weak var detailsStackView: UIStackView!
+  @IBOutlet weak var labelsContainerView: UIView!
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var categoriesLabel: UILabel!
+  @IBOutlet weak var participantsLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: ExpandableLabel!
 }
