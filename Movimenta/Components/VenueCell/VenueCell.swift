0625e07441864808689fca27d5db83cf98ec2054
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-16 23:47:45 +0300

    Add Venue cell

---
+++ Movimenta/Components/VenueCell/VenueCell.swift
@@ -0,0 +1,19 @@
+//
+//  VenueCell.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/16/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class VenueCell: UITableViewCell {
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+}
