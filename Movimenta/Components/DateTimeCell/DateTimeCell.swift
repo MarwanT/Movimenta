56a0775a023c84d483c71bb7c6590610be6e05ce
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-16 11:06:02 +0300

    Add Date Time Cell

---
+++ Movimenta/Components/DateTimeCell/DateTimeCell.swift
@@ -0,0 +1,24 @@
+//
+//  DateTimeCell.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/16/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class DateTimeCell: UITableViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+}
