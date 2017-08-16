25b0291fdf94e0f8434f300f9818b08d65cca98b
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-17 00:58:24 +0300

    Style venue cell layout

--- Movimenta/Components/VenueCell/VenueCell.swift
+++ Movimenta/Components/VenueCell/VenueCell.swift
@@ -18,6 +18,20 @@ class VenueCell: UITableViewCell {
   
   override func awakeFromNib() {
     super.awakeFromNib()
+    applyTheme()
+  }
+  
+  private func applyTheme() {
+    let theme = ThemeManager.shared.current
+    titleLabel.font = theme.font5
+    titleLabel.textColor = theme.color2
+    locationLabel.font = theme.font6
+    locationLabel.textColor = theme.darkTextColor
+    locationLabelTopTitleLabelBottomConstraint.constant = CGFloat(theme.space2)
+    contentView.layoutMargins = UIEdgeInsets(
+      top: 0, left: CGFloat(theme.space7),
+      bottom: 0, right: CGFloat(theme.space7))
+    layoutIfNeeded()
   }
   
   override func setSelected(_ selected: Bool, animated: Bool) {
