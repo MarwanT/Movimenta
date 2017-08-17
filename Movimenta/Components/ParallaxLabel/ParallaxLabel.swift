1de5276f598f1754e59f224f3b27287b009a2bda
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-17 18:29:48 +0300

    Add ParaAdd Parallax Label custom view

---
+++ Movimenta/Components/ParallaxLabel/ParallaxLabel.swift
@@ -0,0 +1,21 @@
+//
+//  ParallaxLabel.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/17/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ParallaxLabel: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
