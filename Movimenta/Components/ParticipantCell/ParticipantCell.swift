d2dfe484965c34a8af6fc554679247345609c551
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-17 12:20:10 +0300

    Add the participant cell

---
+++ Movimenta/Components/ParticipantCell/ParticipantCell.swift
@@ -0,0 +1,19 @@
+//
+//  ParticipantCell.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/17/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ParticipantCell: UITableViewCell {
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+}
