378fb7d8609494bdbc59e5492d7a7794c6b5d4b6
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-17 15:58:53 +0300

    Set the image view background color on selection otherwise the it turns white

--- Movimenta/Components/ParticipantCell/ParticipantCell.swift
+++ Movimenta/Components/ParticipantCell/ParticipantCell.swift
@@ -49,5 +49,6 @@ class ParticipantCell: UITableViewCell {
   
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
+    participantImageView.backgroundColor = ThemeManager.shared.current.color6
   }
 }
