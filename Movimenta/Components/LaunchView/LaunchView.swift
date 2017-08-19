7e3ffecd183e0c7a3d236fcd9763dff26a545fbc
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-19 11:03:51 +0300

    Add Launch View

---
+++ Movimenta/Components/LaunchView/LaunchView.swift
@@ -0,0 +1,12 @@
+//
+//  LaunchView.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/18/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class LaunchView: UIView {
+}
