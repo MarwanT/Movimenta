7b6b539474f838853f835c620e45419bac6db24b
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-21 13:22:08 +0300

    Create Filters Manager class

---
+++ Movimenta/Models/DataModels/Tools/FiltersManager.swift
@@ -0,0 +1,13 @@
+//
+//  FiltersManager.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/21/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class FiltersManager {
+
+}
