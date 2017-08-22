65ae3ad33381f141e1e4f81ab010b92f2435ef58
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-22 19:10:15 +0300

    Add filter model properties

--- Movimenta/Models/DataModels/Filter.swift
+++ Movimenta/Models/DataModels/Filter.swift
@@ -9,4 +9,14 @@
 import Foundation
 
 struct Filter {
+  var dateRange: DateRange? = nil
+  var categories: [Event.Category]? = nil
+  var withinTime: Int? = nil
+  var speakers: [Participant]? = nil
+  var sponsers: [Participant]? = nil
+  var companies: [Participant]? = nil
+  var artists: [Participant]? = nil
+  var organizers: [Participant]? = nil
+  var withinDistance: Double? = nil
+  var showBookmarked: Bool? = nil
 }
