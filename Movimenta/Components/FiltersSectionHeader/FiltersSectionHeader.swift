7a904d8ab80c29ad733623cd9af637a89748128b
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-23 17:52:27 +0300

    Declare Filters Section Header view

---
+++ Movimenta/Components/FiltersSectionHeader/FiltersSectionHeader.swift
@@ -0,0 +1,13 @@
+//
+//  FiltersSectionHeader.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/23/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import SnapKit
+import UIKit
+
+class FiltersSectionHeader: UITableViewHeaderFooterView {
+}
