efedbc45b505c2a3734ac38f99ffeed7405e6dbd
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-24 15:42:29 +0300

    Connect subviews and constraint to outlets

--- Movimenta/Components/DatePickerCell/DatePickerCell.swift
+++ Movimenta/Components/DatePickerCell/DatePickerCell.swift
@@ -12,4 +12,16 @@ class DatePickerCell: UITableViewCell {
   static let identifier: String = DatePickerCell.defaultNibName
   static let nib: UINib = UINib(nibName: identifier, bundle: nil)
   
+  @IBOutlet weak var label: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var separator: UIView!
+  @IBOutlet weak var datePicker: UIDatePicker!
+  
+  @IBOutlet weak var dateLabelTrailingToSuperviewMarginConstraint: NSLayoutConstraint!
+  @IBOutlet weak var separatorTopToDateLabelBottomConstraint: NSLayoutConstraint!
+  @IBOutlet weak var datePickerTopConstraintToSeparatorBottomConstraint: NSLayoutConstraint!
+  @IBOutlet weak var datePickerTrailingToSuperviewMarginConstraint: NSLayoutConstraint!
+  @IBOutlet weak var superviewBottomToSeparatorTopConstraint: NSLayoutConstraint!
+  @IBOutlet weak var superviewBottomToDatePickerBottomConstraint: NSLayoutConstraint!
+  
 }
