e217cc1359787394d9696f21d3f0407801b94848
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-24 15:40:28 +0300

    Create Date Picker Cell

---
+++ Movimenta/Components/DatePickerCell/DatePickerCell.swift
@@ -0,0 +1,12 @@
+//
+//  DatePickerCell.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/24/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerCell: UITableViewCell {
+}
