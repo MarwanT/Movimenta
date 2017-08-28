9ee5194a3aa134e8687c6d23d0dfa64df8d7c5d3
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-29 00:54:17 +0300

    Add Expandable header cell

---
+++ Movimenta/Components/ExpandableHeaderCell/ExpandableHeaderCell.swift
@@ -0,0 +1,12 @@
+//
+//  ExpandableHeaderCell.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/28/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ExpandableHeaderCell: UITableViewCell {
+}
