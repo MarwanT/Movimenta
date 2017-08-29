855b3d1464111a4cb084809315dec6eeb4a40543
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-08-29 16:00:18 +0300

    Add selectable cell

---
+++ Movimenta/Components/SelectableCell/SelectableCell.swift
@@ -0,0 +1,21 @@
+//
+//  SelectableCell.swift
+//  Movimenta
+//
+//  Created by Marwan  on 8/29/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class SelectableCell: UITableViewCell {
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    // Configure the view for the selected state
+  }
+}
