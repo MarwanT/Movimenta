0be1635083fe8bee0ccc56bffc456c169a42e94a
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-04 19:26:34 +0300

    Add slider cell configuration

--- Movimenta/Components/SliderCell/SliderCell.swift
+++ Movimenta/Components/SliderCell/SliderCell.swift
@@ -15,11 +15,23 @@ class SliderCell: UITableViewCell {
   @IBOutlet weak var label: UILabel!
   @IBOutlet weak var slider: UISlider!
   
+  var configuration = Configuration()
+  
   override func awakeFromNib() {
     super.awakeFromNib()
   }
   
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
+  }
+}
+
+extension SliderCell {
+  struct Configuration {
+    var layoutMargins = UIEdgeInsets(
+      top: CGFloat(ThemeManager.shared.current.space7),
+      left: CGFloat(ThemeManager.shared.current.space7),
+      bottom: CGFloat(ThemeManager.shared.current.space7),
+      right: CGFloat(ThemeManager.shared.current.space7))
   }
 }
