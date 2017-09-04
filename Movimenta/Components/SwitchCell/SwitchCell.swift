ee0835e598ef9720e4e0a73bccd50c3178198126
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-04 13:22:46 +0300

    Add the Switch Cell

---
+++ Movimenta/Components/SwitchCell/SwitchCell.swift
@@ -0,0 +1,19 @@
+//
+//  SwitchCell.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/4/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class SwitchCell: UITableViewCell {
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+}
