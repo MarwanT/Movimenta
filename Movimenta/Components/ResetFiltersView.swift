76f43bad7094732fc00d5e9b0e735945e3e5547b
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-05 14:09:21 +0300

    Add reset filters view

---
+++ Movimenta/Components/ResetFiltersView.swift
@@ -0,0 +1,12 @@
+//
+//  ResetFiltersView.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/5/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ResetFiltersView: UIView {
+}
