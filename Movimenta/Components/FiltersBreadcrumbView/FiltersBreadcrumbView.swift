1633e6baee1077720e022c41d192f3a4d33b907b
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-06 15:01:27 +0300

    Add filters breadcrumb view files

---
+++ Movimenta/Components/FiltersBreadcrumbView/FiltersBreadcrumbView.swift
@@ -0,0 +1,12 @@
+//
+//  FiltersBreadcrumbView.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/6/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class FiltersBreadcrumbView: UIView {
+}
