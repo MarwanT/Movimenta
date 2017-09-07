b1cd6f758c230bd1886e77520b61a84a828e362c
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-07 14:29:06 +0300

    Add the padable label class

---
+++ Movimenta/Components/PadableLabel.swift
@@ -0,0 +1,12 @@
+//
+//  PadableLabel.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/7/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class PadableLabel: UILabel {
+}
