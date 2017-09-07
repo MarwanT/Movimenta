bd432ec2a8a5a908a4f710195178d5032f206d06
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-07 16:56:27 +0300

    Declare filters breadcrumb view configuration

--- Movimenta/Components/FiltersBreadcrumbView/FiltersBreadcrumbView.swift
+++ Movimenta/Components/FiltersBreadcrumbView/FiltersBreadcrumbView.swift
@@ -13,3 +13,14 @@ class FiltersBreadcrumbView: UIView {
   @IBOutlet weak var stackView: UIStackView!
   
 }
+
+//MARK: Configuration
+extension FiltersBreadcrumbView {
+  struct Configuration {
+    var backgroundColor: UIColor = ThemeManager.shared.current.color2
+    var foregroundColor: UIColor = ThemeManager.shared.current.lightTextColor
+    var layoutMargin: UIEdgeInsets = UIEdgeInsets(
+      top: 0, left: CGFloat(ThemeManager.shared.current.space7),
+      bottom: 0, right: CGFloat(ThemeManager.shared.current.space7))
+  }
+}
