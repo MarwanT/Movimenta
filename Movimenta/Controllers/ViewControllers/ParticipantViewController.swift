3e7188d58fc26860caa771eb4e345efbf23bb54e
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-11 14:47:27 +0300

    Initialize the participant vc table view

--- Movimenta/Controllers/ViewControllers/ParticipantViewController.swift
+++ Movimenta/Controllers/ViewControllers/ParticipantViewController.swift
@@ -19,5 +19,44 @@ class ParticipantViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    initialize()
+  }
+  
+  private func initialize() {
+    initializeTableView()
+  }
+  
+  private func initializeTableView() {
+    let theme = ThemeManager.shared.current
+    
+    tableView.tableFooterView = UIView(frame: CGRect.zero)
+    
+    tableView.dataSource = self
+    tableView.delegate = self
+    
+    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 100
+    
+    tableView.layoutMargins = UIEdgeInsets(
+      top: 0, left: CGFloat(theme.space7),
+      bottom: 0, right: CGFloat(theme.space7))
+    
+    tableView.separatorStyle = .singleLine
+    tableView.separatorColor = theme.separatorColor
+  }
+}
+
+//MARK: Table View Delegates
+extension ParticipantViewController: UITableViewDelegate, UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
   }
 }
