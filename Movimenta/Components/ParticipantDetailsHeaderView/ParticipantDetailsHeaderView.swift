e8b99e462fd7a4c22d3b957851090e6f704fd0c8
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-11 15:09:10 +0300

    Add participant details header view

---
+++ Movimenta/Components/ParticipantDetailsHeaderView/ParticipantDetailsHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  ParticipantDetailsHeaderView.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/11/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ParticipantDetailsHeaderView: UIView {
+
+}
