34a81b7955a32fe0ca07f5276e6a7e2a67518fa7
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-11 13:27:25 +0300

    Add participant view model class

---
+++ Movimenta/Models/ViewModels/ParticipantViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ParticipantViewModel.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/11/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class ParticipantViewModel {
+  
+}
