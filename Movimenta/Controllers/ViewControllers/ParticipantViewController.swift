39b4744542cd21963899b51b86769400c4b60190
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-11 13:20:19 +0300

    Add participant view controller

---
+++ Movimenta/Controllers/ViewControllers/ParticipantViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ParticipantViewController.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/11/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ParticipantViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
