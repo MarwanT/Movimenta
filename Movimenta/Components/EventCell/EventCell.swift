da3391fe17be3c45d081ca0017f119ee146e3315
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-12 12:45:30 +0300

    Add Event Cell

---
+++ Movimenta/Components/EventCell/EventCell.swift
@@ -0,0 +1,19 @@
+//
+//  EventCell.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/12/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+}
