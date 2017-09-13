3ba68ad4ba9068ade36d8b092b813f8ba03a080d
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-13 13:03:31 +0300

    Link bookmark button to IBAction

--- Movimenta/Components/EventCell/EventCell.swift
+++ Movimenta/Components/EventCell/EventCell.swift
@@ -35,4 +35,7 @@ class EventCell: UITableViewCell {
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
   }
+  
+  @IBAction func bookmarkButtonTouchUpInside(_ sender: Any) {
+  }
 }
