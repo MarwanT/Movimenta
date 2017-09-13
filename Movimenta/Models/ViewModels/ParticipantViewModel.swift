5964fe1a90f04f8f04017da1edeb19b67c6422fc
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-13 13:33:43 +0300

    Add table view data providers methods to participant view model

--- Movimenta/Models/ViewModels/ParticipantViewModel.swift
+++ Movimenta/Models/ViewModels/ParticipantViewModel.swift
@@ -40,3 +40,22 @@ final class ParticipantViewModel {
     return [participant.fullName, url]
   }
 }
+
+//MARK: Table View Methods
+extension ParticipantViewModel {
+  var numberOfRows: Int {
+    return events.count
+  }
+  
+  func values(for indexPath: IndexPath) -> (imageURL: URL?, date: String?, venueName: String?, eventName: String?, categories: String?, time: String?, isBookmarked: Bool?)? {
+    let event = events[indexPath.row]
+    let preferredDateRange = event.preferredDateRange()
+    return (imageURL: event.image,
+            date: preferredDateRange?.displayedShortDate,
+            venueName: event.venue?.name?.uppercased(),
+            eventName: event.title?.capitalized,
+            categories: event.displayedCategoryLabel,
+            time: preferredDateRange?.displayedShortTime,
+            isBookmarked: event.isBookmarked)
+  }
+}
