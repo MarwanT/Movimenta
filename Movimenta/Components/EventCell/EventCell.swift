651db55cbec370d58d2c13665e13ed5bddcfc6d2
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-13 13:07:37 +0300

    Style event cell

--- Movimenta/Components/EventCell/EventCell.swift
+++ Movimenta/Components/EventCell/EventCell.swift
@@ -32,6 +32,49 @@ class EventCell: UITableViewCell {
   
   override func awakeFromNib() {
     super.awakeFromNib()
+    applyTheme()
+  }
+  
+  private func applyTheme() {
+    let theme = ThemeManager.shared.current
+    dateLabel.font = theme.font1
+    venueNameLabel.font = theme.font14
+    eventNameLabel.font = theme.font4
+    categoryLabel.font = theme.font14
+    timeLabel.font = theme.font13
+    dateLabel.textColor = theme.darkTextColor
+    venueNameLabel.textColor = theme.darkTextColor
+    eventNameLabel.textColor = theme.color2
+    categoryLabel.textColor = theme.darkTextColor
+    timeLabel.textColor = theme.darkTextColor
+    bookmarkButton.tintColor = theme.color2
+    refreshBookmarkButton()
+    setLabelsTopPadding()
+    setSideMargins()
+    participantImageView.clipsToBounds = true
+    participantImageView.contentMode = .scaleAspectFill
+    contentView.layoutMargins = configuration.layoutMargins
+    selectedBackgroundView = UIImageView(image: theme.color6.image())
+  }
+  
+  fileprivate func refreshBookmarkButton() {
+    bookmarkButton.setImage(isBookmarked ? #imageLiteral(resourceName: "bookmarkFilled") : #imageLiteral(resourceName: "bookmarkOutline"), for: .normal)
+  }
+  
+  fileprivate func setLabelsTopPadding() {
+    dateLabelTopVerticalSpacing.constant = -15
+    venueNameLabelTopVerticalSpacing.constant = 0
+    eventNameLabelTopVerticalSpacing.constant = 5
+    categoryLabelTopVerticalSpacing.constant = 5
+    timeLabelTopVerticalSpacing.constant = 5
+    contentView.layoutIfNeeded()
+  }
+  
+  fileprivate func setSideMargins() {
+    let theme = ThemeManager.shared.current
+    dateLabelLeadingToImageViewTrailing.constant = CGFloat(theme.space7)
+    dateLabelTrailingToBookmarkButtonLeading.constant = 0
+    contentView.layoutIfNeeded()
   }
   
   override func setSelected(_ selected: Bool, animated: Bool) {
