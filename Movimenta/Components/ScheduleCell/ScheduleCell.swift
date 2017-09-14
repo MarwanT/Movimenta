9aa0b970c0ab3cb5dc8706660b81ef27381e0d15
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-14 14:20:35 +0300

    Add Schedule cell

---
+++ Movimenta/Components/ScheduleCell/ScheduleCell.swift
@@ -0,0 +1,19 @@
+//
+//  ScheduleCell.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/13/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleCell: UICollectionViewCell {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+}
