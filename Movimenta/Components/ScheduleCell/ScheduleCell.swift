151d1a42675ecb21597baa0c3347510fff73e457
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-14 16:20:35 +0300

    Add initialization method call to Schedule Cell

--- Movimenta/Components/ScheduleCell/ScheduleCell.swift
+++ Movimenta/Components/ScheduleCell/ScheduleCell.swift
@@ -15,10 +15,15 @@ class ScheduleCell: UICollectionViewCell {
   
   override init(frame: CGRect) {
     super.init(frame: frame)
+    initialize()
   }
   
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
+    initialize()
+  }
+  
+  private func initialize() {
   }
 }
 
