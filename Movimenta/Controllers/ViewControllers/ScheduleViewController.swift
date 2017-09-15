a0ee1c69f67b44400e11d70514be7885d4df08b6
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-15 13:14:10 +0300

    Initialize dates collection view

--- Movimenta/Controllers/ViewControllers/ScheduleViewController.swift
+++ Movimenta/Controllers/ViewControllers/ScheduleViewController.swift
@@ -19,5 +19,23 @@ class ScheduleViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    initializeCollectionView()
+  }
+  
+  private func initializeCollectionView() {
+    // Featured Content View
+    let interItemSpacing: CGFloat = 10
+    let contentInset = UIEdgeInsets(
+      top: 0, left: CGFloat(ThemeManager.shared.current.space7),
+      bottom: 0, right: CGFloat(ThemeManager.shared.current.space7))
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.scrollDirection = UICollectionViewScrollDirection.horizontal
+    flowLayout.estimatedItemSize = UICollectionViewFlowLayoutAutomaticSize
+    flowLayout.minimumInteritemSpacing = interItemSpacing
+    datesCollectionView.collectionViewLayout = flowLayout
+    datesCollectionView.register(ScheduleCell.self, forCellWithReuseIdentifier: ScheduleCell.identifier)
+    datesCollectionView.backgroundColor = ThemeManager.shared.current.color2
+    datesCollectionView.contentInset = contentInset
+    datesCollectionView.showsHorizontalScrollIndicator = false
   }
 }
