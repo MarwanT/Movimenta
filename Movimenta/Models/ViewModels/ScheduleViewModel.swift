a5cf553cfed04b2aef173af4583618d27c172b85
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-15 12:58:57 +0300

    Add Schedule VM

---
+++ Movimenta/Models/ViewModels/ScheduleViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ScheduleViewModel.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/14/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class ScheduleViewModel {
+}
