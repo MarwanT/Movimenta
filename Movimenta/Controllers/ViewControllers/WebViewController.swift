b7757270ab41d44f4bb758477219bc15b3245a53
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-16 19:36:39 +0300

    Add Safari Web View Controller logic

--- Movimenta/Controllers/ViewControllers/WebViewController.swift
+++ Movimenta/Controllers/ViewControllers/WebViewController.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import SafariServices
+
+class WebViewController {
+  public static func present(url: String, inViewController: UIViewController? = nil) {
+    let presenterViewController: UIViewController = inViewController != nil ? inViewController! : rootViewController
+    let safariVC = SFSafariViewController(url: URL(string: url)!)
+    safariVC.modalPresentationStyle = .overCurrentContext
+    safariVC.modalTransitionStyle = .coverVertical
+    if #available(iOS 10.0, *) {
+      safariVC.preferredControlTintColor = ThemeManager.shared.current.color2
+    }
+    presenterViewController.present(safariVC, animated: true, completion: nil)
+  }
+
+  public static func present(url: URL!, inViewController: UIViewController? = nil) {
+    let presenterViewController: UIViewController = inViewController != nil ? inViewController! : rootViewController
+    let safariVC = SFSafariViewController(url: url)
+    safariVC.modalPresentationStyle = .overCurrentContext
+    safariVC.modalTransitionStyle = .coverVertical
+    if #available(iOS 10.0, *) {
+      safariVC.preferredControlTintColor = ThemeManager.shared.current.color2
+    }
+    presenterViewController.present(safariVC, animated: true, completion: nil)
+  }
+
+  private static var rootViewController: UIViewController {
+    guard let rootViewController = UIApplication.shared.delegate?.window??.rootViewController else {
+      fatalError("No root view controller detected")
+    }
+    return rootViewController
+  }
+}
