b432071f80d849044bd7d6434e66813381a6dd1f
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-17 14:13:38 +0300

    Instantiate InformationListingViewModel

--- Movimenta/Controllers/ViewControllers/InformationListingViewController.swift
+++ Movimenta/Controllers/ViewControllers/InformationListingViewController.swift
@@ -10,6 +10,8 @@ import Foundation
 
 class InformationListingViewController: UIViewController {
 
+  let viewModel = InformationListingViewModel()
+
   override func viewDidLoad() {
     super.viewDidLoad()
     initialize()
