19c93125cc87c546b5e7f0d5f6693ae665010e3c
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-17 14:10:10 +0300

    Add InformationListingViewController file

---
+++ Movimenta/Controllers/ViewControllers/InformationListingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InformationListingViewController.swift
+//  Movimenta
+//
+//  Created by Shafic Hariri on 9/17/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
