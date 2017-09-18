3a2517fbc4641750dc591a4b6efb63a5f2f2dba7
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-18 13:37:50 +0300

    Initialize events table view and populate events

--- Movimenta/Controllers/ViewControllers/ScheduleViewController.swift
+++ Movimenta/Controllers/ViewControllers/ScheduleViewController.swift
@@ -21,6 +21,7 @@ class ScheduleViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     initializeCollectionView()
+    initializeTableView()
     navigateToSelectedDate()
     reloadEventsData()
   }
@@ -42,6 +43,28 @@ class ScheduleViewController: UIViewController {
     datesCollectionView.backgroundColor = ThemeManager.shared.current.color2
     datesCollectionView.contentInset = contentInset
     datesCollectionView.showsHorizontalScrollIndicator = false
+  }
+  
+  private func initializeTableView() {
+    let theme = ThemeManager.shared.current
+    
+    eventsTableView.tableFooterView = UIView(frame: CGRect.zero)
+    
+    eventsTableView.dataSource = self
+    eventsTableView.delegate = self
+    
+    eventsTableView.rowHeight = UITableViewAutomaticDimension
+    eventsTableView.estimatedRowHeight = 100
+    
+    eventsTableView.layoutMargins = UIEdgeInsets(
+      top: 0, left: CGFloat(theme.space7),
+      bottom: 0, right: CGFloat(theme.space7))
+    
+    eventsTableView.separatorStyle = .singleLine
+    eventsTableView.separatorColor = theme.separatorColor
+    eventsTableView.separatorInset = UIEdgeInsets(top: 0, left: 118, bottom: 0, right: 0)
+    
+    eventsTableView.register(EventCell.nib, forCellReuseIdentifier: EventCell.identifier)
   }
   
   fileprivate func navigateToSelectedDate() {
@@ -84,5 +107,35 @@ extension ScheduleViewController: UICollectionViewDelegate, UICollectionViewData
     viewModel.setSelected(for: indexPath)
     reloadEventsData()
     navigateToSelectedDate()
+  }
+}
+
+//MARK: Table View Delegates
+extension ScheduleViewController: UITableViewDelegate, UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.numberOfRows
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: EventCell.identifier, for: indexPath) as? EventCell, let values = viewModel.values(for: indexPath) else {
+      return UITableViewCell()
+    }
+    cell.delegate = self
+    cell.set(imageURL: values.imageURL, date: values.date, venueName: values.venueName, eventName: values.eventName, categories: values.categories, time: values.time, isBookmarked: values.isBookmarked)
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+}
+
+//MARK: Event Cell Delegates
+extension ScheduleViewController: EventCellDelegate {
+  func eventCellDidTapBookmarkButton(_ cell: EventCell) {
   }
 }
