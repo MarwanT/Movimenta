00eb9df3c8270e3f46e22a365fd5d1ff15cb6c1d
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-18 18:55:22 +0300

    Add Bookmarks VM

---
+++ Movimenta/Models/ViewModels/BookmarksViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BookmarksViewModel.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/18/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class BookmarksViewModel {
+  
+}
