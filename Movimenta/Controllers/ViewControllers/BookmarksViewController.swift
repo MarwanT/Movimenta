2ca8baaa73651be73923e9120e9d0453243c678b
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-18 18:52:56 +0300

    Add Bookmarks VC

---
+++ Movimenta/Controllers/ViewControllers/BookmarksViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BookmarksViewController.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/18/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class BookmarksViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
