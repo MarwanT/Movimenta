ba526892df13f090e7710410f33d2bb9367159fd
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-19 12:41:58 +0300

    Add bookmark table view data providers in Bookmarks VC

--- Movimenta/Models/ViewModels/BookmarksViewModel.swift
+++ Movimenta/Models/ViewModels/BookmarksViewModel.swift
@@ -9,5 +9,46 @@
 import Foundation
 
 final class BookmarksViewModel {
+  fileprivate(set) var events = [Event]()
   
+  var numberOfRows: Int {
+    return events.count
+  }
+  
+  func values(for indexPath: IndexPath) -> (imageURL: URL?, date: String?, venueName: String?, eventName: String?, categories: String?, time: String?, isBookmarked: Bool?)? {
+    let event = events[indexPath.row]
+    let preferredDateRange = event.preferredDateRange()
+    return (imageURL: event.image,
+            date: preferredDateRange?.displayedShortDate,
+            venueName: event.venue?.name?.uppercased(),
+            eventName: event.title?.capitalized,
+            categories: event.displayedCategoryLabel,
+            time: preferredDateRange?.displayedShortTime,
+            isBookmarked: event.isBookmarked)
+  }
+  
+  func toggleEventBookmarkStatus(at indexPath: IndexPath) {
+    let event = events[indexPath.row]
+    DataManager.shared.toggleBookmarkStatus(event: event)
+  }
+  
+  func updateBookmarkStatus(of event: Event) -> IndexPath? {
+    guard let index = events.index(of: event) else {
+      return nil
+    }
+    return IndexPath(row: index, section: 0)
+  }
+  
+  func loadEvents() {
+    refreshEvents()
+  }
+  
+  private func refreshEvents() {
+    events.removeAll()
+    events.append(contentsOf: DataManager.shared.bookmarkedEvents)
+  }
+  
+  func event(for indexPath: IndexPath) -> Event {
+    return events[indexPath.row]
+  }
 }
