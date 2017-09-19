9fc9b8543c863c3ab7dac8e14e0d2b8cbf9020b7
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-19 12:44:09 +0300

    Initialize bookmarks table view

--- Movimenta/Controllers/ViewControllers/BookmarksViewController.swift
+++ Movimenta/Controllers/ViewControllers/BookmarksViewController.swift
@@ -15,5 +15,48 @@ class BookmarksViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    initializeTableView()
+  }
+  
+  private func initializeTableView() {
+    let theme = ThemeManager.shared.current
+    
+    tableView.tableFooterView = UIView(frame: CGRect.zero)
+    
+    tableView.dataSource = self
+    tableView.delegate = self
+    
+    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 100
+    
+    tableView.layoutMargins = UIEdgeInsets(
+      top: 0, left: CGFloat(theme.space7),
+      bottom: 0, right: CGFloat(theme.space7))
+    
+    tableView.separatorStyle = .singleLine
+    tableView.separatorColor = theme.separatorColor
+    tableView.separatorInset = UIEdgeInsets(top: 0, left: 118, bottom: 0, right: 0)
+    
+    tableView.register(EventCell.nib, forCellReuseIdentifier: EventCell.identifier)
+  }
+}
+
+//MARK: Table View Controller
+extension BookmarksViewController: UITableViewDelegate, UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.numberOfRows
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: EventCell.identifier, for: indexPath) as? EventCell, let values = viewModel.values(for: indexPath) else {
+      return UITableViewCell()
+    }
+    cell.delegate = self
+    cell.set(imageURL: values.imageURL, date: values.date, venueName: values.venueName, eventName: values.eventName, categories: values.categories, time: values.time, isBookmarked: values.isBookmarked)
+    return cell
   }
 }
