d815c0b79681ddc490c4cf87d2b8b2dcc8655388
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-20 16:23:47 +0300

    Link no bookmarks view subviews wth outlets

--- Movimenta/Components/NoBookmarksView/NoBookmarksView.swift
+++ Movimenta/Components/NoBookmarksView/NoBookmarksView.swift
@@ -9,4 +9,7 @@
 import UIKit
 
 class NoBookmarksView: UIView {
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var contentLabel: UILabel!
 }
