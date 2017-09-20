9866f7c7a2391b90e73dafc14f6b62d0971f9380
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-20 16:13:03 +0300

    Add no bookmarks view

---
+++ Movimenta/Components/NoBookmarksView/NoBookmarksView.swift
@@ -0,0 +1,12 @@
+//
+//  NoBookmarksView.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/20/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class NoBookmarksView: UIView {
+}
