b601b7d9e95b5f96fe43fd3110107ddbadf0419f
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-21 14:48:41 +0300

    Add Venue View Model

---
+++ Movimenta/Models/ViewModels/VenueViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  VenueViewModel.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/21/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+final class VenueViewModel {
+  
+}
