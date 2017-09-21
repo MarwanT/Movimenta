835a96645191d303250f78b356679b40e389fd91
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-21 16:34:56 +0300

    Add Venue Details Header View

---
+++ Movimenta/Components/VenueDetailsHeaderView/VenueDetailsHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  VenueDetailsHeaderView.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/21/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class VenueDetailsHeaderView: UIView {
+}
