ae605931e20fde9a066327cd540931beb215f0dd
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-21 14:38:28 +0300

    Add Venue View Controller

---
+++ Movimenta/Controllers/ViewControllers/VenueViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VenueViewController.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/21/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class VenueViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
