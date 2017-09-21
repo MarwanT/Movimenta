8d45d6c9fa27a13e337e17bd2cabb39becdc9203
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-21 15:05:31 +0300

    Initialize table view

--- Movimenta/Controllers/ViewControllers/VenueViewController.swift
+++ Movimenta/Controllers/ViewControllers/VenueViewController.swift
@@ -15,5 +15,52 @@ class VenueViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    initialize()
+  }
+  
+  private func initialize() {
+    initializeTableView()
+  }
+  
+  private func initializeTableView() {
+    let theme = ThemeManager.shared.current
+    
+    initializeTableViewHeader()
+    
+    tableView.tableFooterView = UIView(frame: CGRect.zero)
+    
+    tableView.dataSource = self
+    tableView.delegate = self
+    
+    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 100
+    
+    tableView.layoutMargins = UIEdgeInsets(
+      top: 0, left: CGFloat(theme.space7),
+      bottom: 0, right: CGFloat(theme.space7))
+    
+    tableView.separatorStyle = .singleLine
+    tableView.separatorColor = theme.separatorColor
+    tableView.separatorInset = UIEdgeInsets(top: 0, left: 118, bottom: 0, right: 0)
+    
+    tableView.register(EventCell.nib, forCellReuseIdentifier: EventCell.identifier)
+  }
+  
+  private func initializeTableViewHeader() {
+    // TODO:
+  }
+}
+
+extension VenueViewController: UITableViewDelegate, UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 0
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
   }
 }
