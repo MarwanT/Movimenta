ecb4b2e1df2b371c72efea2bcaa37da01888001d
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-22 17:48:18 +0300

    Add Gallery Page View Controller

---
+++ Movimenta/Components/GalleryViewController/GalleryPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GalleryPageViewController.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/22/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class GalleryPageViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
