1d91b1c4b8938b755c391cc6479ab5627c830d57
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-22 13:04:15 +0300

    Style venue details header view subviews

--- Movimenta/Components/VenueDetailsHeaderView/VenueDetailsHeaderView.swift
+++ Movimenta/Components/VenueDetailsHeaderView/VenueDetailsHeaderView.swift
@@ -19,4 +19,22 @@ class VenueDetailsHeaderView: UIView {
   @IBOutlet weak var labelsContainerView: UIView!
   @IBOutlet weak var nameLabel: UILabel!
   @IBOutlet weak var addressLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    applyTheme()
+  }
+  
+  private func applyTheme() {
+    let theme = ThemeManager.shared.current
+    contentView.layoutMargins = UIEdgeInsets(
+      top: CGFloat(theme.space7), left: CGFloat(theme.space7),
+      bottom: CGFloat(theme.space7), right: CGFloat(theme.space7))
+    nameLabel.font = theme.font1
+    nameLabel.textColor = theme.darkTextColor
+    addressLabel.font = theme.font6
+    addressLabel.textColor = theme.darkTextColor
+    mapImageView.backgroundColor = theme.color6
+    segmentedControl.tintColor = theme.color2
+  }
 }
