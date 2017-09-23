5129ce8f1546b051abc4d71cc55859fb2846bdb5
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-23 18:32:56 +0300

    Develop the Augment VC skeleton

--- Movimenta/Controllers/ViewControllers/AugmentedViewController.swift
+++ Movimenta/Controllers/ViewControllers/AugmentedViewController.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2017 Keeward. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AugmentedViewController: UIViewController {
+
+  static func instance() -> AugmentedViewController {
+    return Storyboard.Root.instantiate(AugmentedViewController.self)
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    initialize()
+  }
+
+  private func initialize() {
+    applyTheme()
+    setupView()
+  }
+
+  private func applyTheme() {
+    //TODO: apply Theme
+  }
+
+  private func setupView() {
+    //TODO: setup View
+  }
+}
+
