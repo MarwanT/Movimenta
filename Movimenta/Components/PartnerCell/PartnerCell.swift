a5b4e1128bac2bc5519bca9eddd963168b389e86
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-23 14:55:29 +0300

    Add partner cell swift and xib files

---
+++ Movimenta/Components/PartnerCell/PartnerCell.swift
@@ -0,0 +1,18 @@
+//
+//  PartnerCell.swift
+//  Movimenta
+//
+//  Created by Shafic Hariri on 9/23/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class PartnerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
