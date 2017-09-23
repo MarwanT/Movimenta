96e475e2728e886d9dc85ed7a1ecf42fe4842fc9
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-23 14:57:58 +0300

    Develop the partner view model logic

--- Movimenta/Models/ViewModels/PartnersViewModel.swift
+++ Movimenta/Models/ViewModels/PartnersViewModel.swift
@@ -9,5 +9,41 @@
 import Foundation
 
 class PartnersViewModel {
+  fileprivate var data: [PartnerGroup] = []
 
+  init() {
+    self.data = DataManager.shared.partnerGroups
+  }
+
+  func itemForSection(section: Int) -> PartnerGroup? {
+    guard data.count > section else {
+      return nil
+    }
+    let item = data[section]
+    return item
+  }
+
+  func itemFor(row: Int, section: Int) -> Partner? {
+    let section = itemForSection(section: section)
+    guard let partners = section?.partners, partners.count > row else {
+      return nil
+    }
+    let item = partners[row]
+    return item
+  }
+}
+
+//MARK: Table view helpers
+extension PartnersViewModel {
+  func numberOfSections() -> Int {
+    return data.count
+  }
+
+  func numberOfRowForSection(section: Int) -> Int {
+    return data[section].partners?.count ?? 0
+  }
+
+  func itemAtIndexPath(indexPath: IndexPath) -> Partner? {
+    return itemFor(row: indexPath.row, section: indexPath.section)
+  }
 }
