a576cba29ebf4211a69218cb3ef53f81d246f1c9
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-23 15:01:00 +0300

    Add PartnerSectionCell files

---
+++ Movimenta/Components/PartnerSectionCell/PartnerSectionCell.swift
@@ -0,0 +1,21 @@
+//
+//  PartnerSectionCell.swift
+//  Movimenta
+//
+//  Created by Shafic Hariri on 9/23/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class PartnerSectionCell: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
