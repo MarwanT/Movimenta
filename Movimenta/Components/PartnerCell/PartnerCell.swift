b1a2c408185b9f018104c2786c5f088b18337a1a
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-23 14:56:57 +0300

    Develop the partner cell with styling

--- Movimenta/Components/PartnerCell/PartnerCell.swift
+++ Movimenta/Components/PartnerCell/PartnerCell.swift
@@ -12,9 +12,44 @@ class PartnerCell: UITableViewCell {
   static let identifier: String = PartnerCell.defaultNibName
   static let nib: UINib = UINib(nibName: identifier, bundle: nil)
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+  @IBOutlet weak var partnerImageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
+
+  var configuration = Configuration()
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    contentView.layoutMargins = configuration.layoutMargins
+    applyTheme()
+  }
+
+  private func applyTheme() {
+    let theme = ThemeManager.shared.current
+    titleLabel.font = theme.font4
+    descriptionLabel.font = theme.font6
+
+    titleLabel.textColor = theme.darkTextColor
+    descriptionLabel.textColor = theme.color2
+  }
+
+  public func setup(title: String?, description: String?, imageURL: String?) {
+    titleLabel.text = title
+    descriptionLabel.text = description
+
+    if let imageURL = imageURL, let url = URL(string: imageURL) {
+      partnerImageView.sd_setImage(with: url)
     }
-    
+  }
+}
+
+//MARK: Configuration
+extension PartnerCell {
+  struct Configuration {
+    var layoutMargins = UIEdgeInsets(
+      top: CGFloat(ThemeManager.shared.current.space4),
+      left: CGFloat(ThemeManager.shared.current.space7),
+      bottom: CGFloat(ThemeManager.shared.current.space4),
+      right: CGFloat(ThemeManager.shared.current.space7))
+  }
 }
