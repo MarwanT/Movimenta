d9480b860beaf0c8e4afd272bbe4bf2b61b5571f
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-23 14:59:12 +0300

    Setup the table view in the partners vc

--- Movimenta/Controllers/ViewControllers/PartnersViewController.swift
+++ Movimenta/Controllers/ViewControllers/PartnersViewController.swift
@@ -21,16 +21,59 @@ class PartnersViewController: UIViewController {
 
   private func initialize() {
     //All Initializations and Setup
+    tableView.delegate = self
+    tableView.dataSource = self
     applyTheme()
     setupView()
   }
 
   private func setupView() {
-    //TODO: Addtional needed setup
+    let theme = ThemeManager.shared.current
+
+    //Addtional needed setup
+    tableView.tableFooterView = UIView(frame: CGRect.zero)
+    tableView.separatorStyle = .singleLine
+    tableView.separatorColor = theme.separatorColor
+    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 220
+    tableView.sectionHeaderHeight = UITableViewAutomaticDimension
+    tableView.estimatedSectionHeaderHeight = 40
+
+    tableView.register(PartnerCell.nib, forCellReuseIdentifier: PartnerCell.identifier)
   }
 
   private func applyTheme() {
     //TODO: Apply style
+  }
+}
+
+extension PartnersViewController: UITableViewDataSource, UITableViewDelegate {
+  public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.numberOfRowForSection(section: section)
+  }
+
+  public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: PartnerCell.identifier, for: indexPath) as? PartnerCell else {
+      return UITableViewCell()
+    }
+    guard let item = viewModel.itemAtIndexPath(indexPath: indexPath) else {
+      return cell
+    }
+    cell.setup(title: item.name, description: item.description, imageURL: item.image)
+    return cell
+  }
+  
+  public func numberOfSections(in tableView: UITableView) -> Int {
+    return viewModel.numberOfSections()
+  }
+
+  public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return viewModel.itemForSection(section: section)?.title ?? "No Title"
+  }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    //TODO: action
   }
 }
 
