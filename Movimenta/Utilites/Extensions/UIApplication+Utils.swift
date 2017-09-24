ef5db505ab90e517444eee416c95174f332e3658
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-24 20:53:58 +0300

    Add file for adding extensions to UIApplication

---
+++ Movimenta/Utilites/Extensions/UIApplication+Utils.swift
@@ -0,0 +1,12 @@
+//
+//  UIApplication+Utils.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/22/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+extension UIApplication {
+}
