60ea7268ed20926455aac8fc5e19068f1e7ff74a
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-24 11:59:27 +0300

    Develop the youtube vc

--- Movimenta/Controllers/ViewControllers/YoutubeViewController.swift
+++ Movimenta/Controllers/ViewControllers/YoutubeViewController.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2017 Keeward. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import youtube_ios_player_helper
+
+class YoutubeViewController: UIViewController {
+
+  static func instance() -> YoutubeViewController {
+    return Storyboard.Root.instantiate(YoutubeViewController.self)
+  }
+
+  @IBOutlet weak var youtubePlayerView: YTPlayerView!
+
+  private var videoId: String?
+
+  func initialize(with youtubeVideoId: String) {
+    self.videoId = youtubeVideoId
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if let videoId = videoId {
+      youtubePlayerView.load(withVideoId: videoId)
+    }
+  }
+}
