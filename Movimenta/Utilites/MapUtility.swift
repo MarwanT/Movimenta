85123df0a8cf3b90c79c3121e60bc1bfc17dac70
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-24 20:57:57 +0300

    Add Map Utility Class

---
+++ Movimenta/Utilites/MapUtility.swift
@@ -0,0 +1,13 @@
+//
+//  MapUtility.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/22/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import CoreLocation
+import Foundation
+
+class MapUtility {
+}
