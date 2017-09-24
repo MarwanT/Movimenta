1b75f225c621f10e6765968797446469c933120f
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-24 20:59:27 +0300

    Add method to Map Utility for retrieving an image of the map given coordinates

--- Movimenta/Utilites/MapUtility.swift
+++ Movimenta/Utilites/MapUtility.swift
@@ -10,4 +10,22 @@ import CoreLocation
 import Foundation
 
 class MapUtility {
+  class func getStaticMapImageURL(for coordinates: CLLocationCoordinate2D, with configuration: MapImageConfiguration = MapImageConfiguration()) -> URL? {
+    return URL(string: getStaticMapImageURLString(for: coordinates, with: configuration))
+  }
+  
+  class func getStaticMapImageURLString(for coordinates: CLLocationCoordinate2D, with configuration: MapImageConfiguration = MapImageConfiguration()) -> String {
+    return "\(configuration.staticMapBaseURL)?\(configuration.coordinates)=\(coordinates.latitude),\(coordinates.longitude)&\(configuration.zoom)&\(configuration.size)&\(configuration.sensor)&\(configuration.marker)%7C\(coordinates.latitude),\(coordinates.longitude)"
+  }
+}
+
+extension MapUtility {
+  struct MapImageConfiguration {
+    fileprivate var staticMapBaseURL = "http://maps.google.com/maps/api/staticmap"
+    var coordinates = "center"
+    var zoom = "zoom=15"
+    var size = "size=600x600"
+    var sensor = "sensor=false"
+    var marker = "markers=icon:https://image.ibb.co/jFmfR5/icon_pin_map_selected_blue_copy.png"
+  }
 }
