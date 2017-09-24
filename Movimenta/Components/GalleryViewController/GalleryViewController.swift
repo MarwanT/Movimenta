84ee629192eb24d984df64e2263461196605bbc6
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-24 17:05:47 +0300

    Initialize Gallery VC

--- Movimenta/Components/GalleryViewController/GalleryViewController.swift
+++ Movimenta/Components/GalleryViewController/GalleryViewController.swift
@@ -13,6 +13,30 @@ class GalleryViewController: UIPageViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    initialize()
+  }
+  
+  private func initialize() {
+    dataSource = self
+    delegate = self
+    view.backgroundColor = ThemeManager.shared.current.color2
+  }
+}
+
+
+// MARK: Page View Controller Delegates
+extension GalleryViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+  func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+    guard let currentViewControllerIndex = pagesViewControllers.index(of: viewController) else {
+      return nil
+    }
+      return nil
+  
+  func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+    guard let currentViewControllerIndex = pagesViewControllers.index(of: viewController) else {
+      return nil
+    }
+      return nil
   }
 }
 
