50d6630430272544dcbc3466a4b0fb06858d3ef2
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-09-26 18:24:54 +0300

    Add Bookmark Notification Manager class

---
+++ Movimenta/Models/DataModels/Tools/BookmarkNotificationManager.swift
@@ -0,0 +1,13 @@
+//
+//  BookmarkNotificationManager.swift
+//  Movimenta
+//
+//  Created by Marwan  on 9/25/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import Foundation
+
+class BookmarkNotificationManager: NSObject {
+}
+
