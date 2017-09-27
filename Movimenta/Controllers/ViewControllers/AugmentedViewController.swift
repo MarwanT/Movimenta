30fd0741dce4d06741703e7bfb6cbd7e12627162
Shafic Hariri <shafic.hariri@keeward.com>
2017-09-27 10:50:03 +0300

    Add the camera permissions checks

--- Movimenta/Controllers/ViewControllers/AugmentedViewController.swift
+++ Movimenta/Controllers/ViewControllers/AugmentedViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class AugmentedViewController: UIViewController {
 
@@ -55,9 +57,73 @@ class AugmentedViewController: UIViewController {
 
   // MARK: Actions
   func arButtonTouchUpInside(_ sender: UIView) {
-    let arViewController = ARViewController()
-    arViewController.targetDelegate = self
-    self.navigationController?.pushViewController(arViewController, animated: true)
+    checkCameraPermissions { (success) in
+      if(success) {
+        let arViewController = ARViewController()
+        arViewController.targetDelegate = self
+        self.navigationController?.pushViewController(arViewController, animated: true)
+      }
+    }
+  }
+
+  /**
+   * Validate the permissions of the camera and
+   * library, if the user do not accept these
+   * permissions, it shows an view that notifies
+   * the user that it not allow the permissions.
+   */
+  private func checkCameraPermissions(completionBlock: @escaping (_ success: Bool) -> ()) {
+    let deviceHasCamera = UIImagePickerController.isSourceTypeAvailable(.camera)
+    if(deviceHasCamera) {
+      let authStatus = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+
+      switch authStatus {
+      case .authorized: completionBlock(true)
+      case .denied: alertPromptToAllowCameraAccessViaSettings(completionBlock: completionBlock)
+      case .notDetermined: permissionPrimeCameraAccess(completionBlock: completionBlock)
+      default: permissionPrimeCameraAccess(completionBlock: completionBlock)
+      }
+
+    } else {
+      hasNoCameraAlert(completionBlock: completionBlock)
+    }
+  }
+
+  func hasNoCameraAlert(completionBlock: @escaping (_ success: Bool) -> ()) {
+    let alertController = UIAlertController(title: Strings.device_has_no_camera_alert_title(), message: Strings.device_has_no_camera_alert_message(), preferredStyle: .alert)
+    let defaultAction = UIAlertAction(title: Strings.ok(), style: .default, handler: { (alert) in
+      completionBlock(false)
+    })
+    alertController.addAction(defaultAction)
+
+    present(alertController, animated: true, completion: nil)
+  }
+
+  func alertPromptToAllowCameraAccessViaSettings(completionBlock: @escaping (_ success: Bool) -> ()) {
+    let alert = UIAlertController(title: Strings.allow_camera_access_via_settings_alert_title(), message: Strings.allow_camera_access_via_settings_alert_message(), preferredStyle: .alert )
+    alert.addAction(UIAlertAction(title: Strings.allow_camera_access_via_settings_alert_button(), style: .cancel) { alert in
+      completionBlock(false)
+      UIApplication.openUrl(url: URL(string: UIApplicationOpenSettingsURLString)!)
+    })
+    present(alert, animated: true, completion: nil)
+  }
+
+
+  func permissionPrimeCameraAccess(completionBlock: @escaping (_ success: Bool) -> ()) {
+    let alert = UIAlertController( title: Strings.allow_camera_access_via_settings_alert_title(), message: Strings.allow_camera_access_via_settings_alert_message(), preferredStyle: .alert )
+    let allowAction = UIAlertAction(title: Strings.allow_camera_access_alert_button_yes(), style: .default, handler: { (alert) -> Void in
+      AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { granted in
+        DispatchQueue.main.async {
+          completionBlock(true)
+        }
+      })
+    })
+    alert.addAction(allowAction)
+    let declineAction = UIAlertAction(title: Strings.allow_camera_access_alert_button_no(), style: .cancel) { (alert) in
+      completionBlock(false)
+    }
+    alert.addAction(declineAction)
+    present(alert, animated: true, completion: nil)
   }
 }
 
