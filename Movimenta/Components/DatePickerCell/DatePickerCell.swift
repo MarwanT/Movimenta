51c45ea84ba36dfc324f77d6b53698e5bd90a722
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-10-14 23:15:56 +0300

    Define date picker cell configuration structure

--- Movimenta/Components/DatePickerCell/DatePickerCell.swift
+++ Movimenta/Components/DatePickerCell/DatePickerCell.swift
@@ -11,11 +11,25 @@ import UIKit
 class DatePickerCell: UITableViewCell {
   @IBOutlet weak var datePicker: UIDatePicker!
   
+  var configuration = Configuration()
+  
   override func awakeFromNib() {
     super.awakeFromNib()
   }
   
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
+  }
+}
+
+//MARK: Configuration declaration
+extension DatePickerCell {
+  struct Configuration {
+    var layoutMargins = UIEdgeInsets(
+      top: CGFloat(ThemeManager.shared.current.space8),
+      left: CGFloat(ThemeManager.shared.current.space7),
+      bottom: CGFloat(ThemeManager.shared.current.space8),
+      right: CGFloat(ThemeManager.shared.current.space7))
+    var mode: UIDatePickerMode = .date
   }
 }
