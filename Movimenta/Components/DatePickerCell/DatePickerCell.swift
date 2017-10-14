61a69e3d987d9ee9d60d392a1fe24ef0c61f40a8
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-10-14 23:04:22 +0300

    Add Date picker cell

---
+++ Movimenta/Components/DatePickerCell/DatePickerCell.swift
@@ -0,0 +1,19 @@
+//
+//  DatePickerCell.swift
+//  Movimenta
+//
+//  Created by Marwan  on 10/14/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerCell: UITableViewCell {
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+}
