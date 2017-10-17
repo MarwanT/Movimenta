5c1280a5ff8dceed20e7b5f11986cc833bce2019
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-10-17 19:18:07 +0300

    Add content edge insets to the email us buttons

--- Movimenta/Themes/MovimentaTheme/MovimentaTheme.swift
+++ Movimenta/Themes/MovimentaTheme/MovimentaTheme.swift
@@ -138,6 +138,7 @@ final class MovimentaTheme: Theme {
     button.setTitleColor(color2, for: .highlighted)
     button.setBackgroundImage(lightTextColor.image(), for: .highlighted)
     button.backgroundColor = color2
+    button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
     addButtonBorders(button: button)
     addButtonConstraints(button: button)
   }
@@ -149,6 +150,7 @@ final class MovimentaTheme: Theme {
     button.setTitleColor(lightTextColor, for: .highlighted)
     button.setBackgroundImage(color2.image(), for: .highlighted)
     button.backgroundColor = lightTextColor
+    button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
     addButtonBorders(button: button)
     addButtonConstraints(button: button)
   }
