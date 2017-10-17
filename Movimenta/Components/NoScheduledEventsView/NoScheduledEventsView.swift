1c22e07bd64781c87583760b53d082670a1aaa72
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-10-17 16:49:29 +0300

    Configure no schedule events view

--- Movimenta/Components/NoScheduledEventsView/NoScheduledEventsView.swift
+++ Movimenta/Components/NoScheduledEventsView/NoScheduledEventsView.swift
@@ -16,4 +16,25 @@ class NoScheduledEventsView: UIView {
     return UINib(nibName: NoScheduledEventsView.defaultNibName, bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! NoScheduledEventsView
   }
   
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    initialize()
+    applyTheme()
+  }
+  
+  private func initialize() {
+    titleLabel.text = Strings.no_events_found()
+    contentLabel.text = Strings.no_events_happening_on_day()
+  }
+  
+  private func applyTheme() {
+    let theme = ThemeManager.shared.current
+    titleLabel.font = theme.font7
+    titleLabel.textColor = theme.darkTextColor
+    contentLabel.font = theme.font6
+    contentLabel.textColor = theme.darkTextColor
+    layoutMargins = UIEdgeInsets(
+      top: 0, left: CGFloat(theme.space4),
+      bottom: 0, right: CGFloat(theme.space4))
+  }
 }
