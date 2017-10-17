7a927bb5bd6b8e7f8333f81a6264c97d3daf1682
Marwan Toutounji <marwan.toutounji@keeward.com>
2017-10-17 16:41:43 +0300

    Add no schedule events view

---
+++ Movimenta/Components/NoScheduledEventsView/NoScheduledEventsView.swift
@@ -0,0 +1,12 @@
+//
+//  NoScheduledEventsView.swift
+//  Movimenta
+//
+//  Created by Marwan  on 10/17/17.
+//  Copyright © 2017 Keeward. All rights reserved.
+//
+
+import UIKit
+
+class NoScheduledEventsView: UIView {
+}
